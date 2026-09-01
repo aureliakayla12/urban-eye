e285a89917bd8bd94e3c15b384dbe8138441b27a
Kayla Aurelia Sakinah <kayla.aurelia2580@smk.belajar.id>
2026-09-01 23:06:42 +0700

    Chore : Configure DaisyUI and Lucide-Vue-Next dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import daisyui from 'daisyui';
 
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -18,5 +18,5 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, daisyui],
 };
